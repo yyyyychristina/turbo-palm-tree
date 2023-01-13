34e28abb36f11f7d5d8618383e85422d4a01fb42
yyyyychristina <114827915+yyyyychristina@users.noreply.github.com>
2023-01-13 00:15:10 -0800

    Add files via upload

---
+++ MyFirstClassHelloWorld.java
@@ -0,0 +1,15 @@
+// Assignment 01
+// 8/29/2022
+// Chingwing Yeung
+// CIS016 
+
+package my1stPackage;
+
+public class MyFirstClassHelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World.");
+	}
+
+}
